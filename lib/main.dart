b5438eab4bb5d4520b1a8a224efd19bd68f62222
denys <denys.zlyvko@outlook.com>
2026-04-08 08:42:45 +0300

    main.dart changes

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/profile_screen.dart';
+
+void main() {
+  runApp(const SpotifyCloneApp());
+}
+
+class SpotifyCloneApp extends StatelessWidget {
+  const SpotifyCloneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify Clone',
+      theme: ThemeData.dark(),
+      home: const MainNavigationScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.green,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Плейліст',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Профіль',
+          ),
+        ],
+      ),
+    );
+  }
+}
